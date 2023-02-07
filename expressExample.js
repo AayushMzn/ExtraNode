914fe55c2a06035f5994f42b3583edbb0d8f0b60
d3m0nk3y <aayushmaharjan20@gmail.com>
2023-02-07 12:31:32 +0545

    new changes

--- expressExample.js
+++ expressExample.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express();
+const cors = require("cors");
+const bodyParser =  require("body-parser");
 const port = 3080;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors({ origin:"*"}));
+app.set("port", port);
 // app.get('/get-hello', (req,res)=>{
     
 //     const a = req.query.a;
@@ -13,9 +19,17 @@ const port = 3080;
 // })
 
 app.post('/', (req,res)=>{
-
+    const num1 = req.body.a;
+    const num2 = req.body.b;
+    const val=sum(num1,num2);
+    res.status(200).send('sum='+val)
 })
 
+function sum(a,b){
+    const s1 = parseInt(a);
+    const s2 = parseInt(b);
+    return(s1+s2);
+}
 
 app.listen(port, () => {
     console.log(`Example of ${port}`)
