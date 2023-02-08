5b50653a7bfb5a073ad6343f8707aa0840f885aa
d3m0nk3y <aayushmaharjan20@gmail.com>
2023-02-08 08:57:16 +0545

    login

--- calculationHelper.js
+++ calculationHelper.js
@@ -1,15 +1,45 @@
+const { request } = require("http");
+const httpStatus = require("http-status");
+
 //-----------calculation function in different file---------------------
 ((calculationHelper) =>{
+
     calculationHelper.sum= function(a,b){
         
         return a+b;
     }
+
     calculationHelper.diff= function(a,b){
-        
         setTimeout(() => {
             console.log("timeOut");
             // const diff = a-b;
         }, 5000);
         return a-b;
+    }
+
+    calculationHelper.login = async(request) =>{
+        const isValid = await calculationHelper.loginValidation(request);
+        if(isValid==false){
+            return {
+                status: httpStatus.BAD_GATEWAY, message:"username is required"
+            }
+        }
+        if(request.user=="abc"){
+            console.log("success");
+            const response = "success";
+            return response;
+        }
+        else{
+            console.log("unsuccess");
+            const response="unsuccessful";
+            return response;
+        }
+    }
+
+    calculationHelper.loginValidation= async(request) =>{
+        if(request.user){
+            return true;
+        }
+        return false;
     }
 })(module.exports)
--- expressExample.js
+++ expressExample.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const cors = require("cors");//secuity
 const bodyParser =  require("body-parser");
-const calculationHelper = require("./calculationHelper"); 
+const {login} = require("./calculationHelper"); 
 const port = 3080;
 
 app.use(bodyParser.json());
@@ -15,20 +15,26 @@ app.set("port", port);
 //     const a = req.query.a;
 //     const b = req.query.b;
 //     const send = parseInt(a)+parseInt(b);
-//     console.log(send)
-//     res.status(200).send('sum='+send)
+//     console.log(send);
+//     res.status(200).send('sum='+send);
 // })
 
 //---------------------calculation using post from different function in different file----------------------
+// app.post('/', (req,res)=>{
+//     const num1 = parseInt(req.body.a);
+//     const num2 = parseInt(req.body.b);
+//     const val = calculationHelper.sum(num1,num2);
+//     const val2 = calculationHelper.diff(num2,num1);
+//     res.status(200).send('sum='+val+'\ndifference='+val2);
+// })
+
 app.post('/', (req,res)=>{
-    const num1 = parseInt(req.body.a);
-    const num2 = parseInt(req.body.b);
-    const val = calculationHelper.sum(num1,num2);
-    const val2 = calculationHelper.diff(num2,num1);
-    res.status(200).send('sum='+val+'\ndifference='+val2)
+    const response = login(req.body);
+    console.log(response);
+    res.status(200).send(response);
 })
 
 app.listen(port, () => {
-    console.log(`Example of ${port}`)
+    console.log(`Example of ${port}`);
 }
 )
